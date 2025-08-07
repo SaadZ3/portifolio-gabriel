2352202551d80387decaf0c6b68fd05f59cc647a
SaadZ3 <saadmu139@gmail.com>
2025-08-06 23:35:43 -0300

    commit

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 // Header scroll effect
-    window.addEventListener('scroll', () => {
-      const header = document.getElementById('header');
-      if (window.scrollY > 100) {
-        header.classList.add('scrolled');
-      } else {
-        header.classList.remove('scrolled');
-      }
-    });
+window.addEventListener('scroll', () => {
+  const header = document.getElementById('header');
+  if (window.scrollY > 100) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+});
+
+document.getElementById('year').textContent = new Date().getFullYear();
+  
